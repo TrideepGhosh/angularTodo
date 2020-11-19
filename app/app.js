5b3b9c03c947c7677044eb8d0406bd76fcaef3e1
Trideep Ghosh <trideep.ghosh@e9.co.uk>
2020-11-19 13:49:26 +0530

    Initail commit of the angular Todo App

---
+++ app/app.js
@@ -0,0 +1,49 @@
+let nameApp = angular.module('nameApp',['ngRoute'])
+
+nameApp.config(['$routeProvider',($routeProvider)=>{
+    $routeProvider
+    .when('/home',{
+        templateUrl:'views/home.html'
+    })
+    .when('/list',{
+        templateUrl:'views/todo.html',
+        controller: 'todoController'
+    })
+    .otherwise({
+        redirectTo:'/home'
+    })
+}])
+
+nameApp.controller('todoController',['$scope',function($scope){
+    $scope.removeTodo = (todo) =>{
+        let removeTodo = $scope.todos.indexOf(todo)
+        $scope.todos.splice(removeTodo,1)
+    }
+
+    $scope.addDetails = () =>{
+        $scope.todos.push({
+            list:$scope.newTodo.list,
+            style:'none'
+        })
+        $scope.newTodo.list = ''
+    }
+
+    $scope.statusOfTodo = (todo) =>{
+        let index = $scope.todos.indexOf(todo)
+        if (todo.style == 'none') {
+            $scope.todos[index].style = 'line-through'
+        }else{
+            $scope.todos[index].style = 'none'
+        }
+    }
+    $scope.todos=[
+        // {
+        //     list:'test',
+        //     style:'none'
+        // },
+        // {
+        //     list:'test2',
+        //     style:'none'
+        // }
+        ]
+}])
